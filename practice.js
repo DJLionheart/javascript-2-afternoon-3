c438eb8f3c3b07f6f95fa752d2e8a0b3734ab850
Paul Peterson <paulpeterson@Pauls-MacBook-Pro.local>
2018-03-20 15:37:05 -0600

    final

--- practice.js
+++ practice.js
@@ -170,7 +170,13 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+function getUserById(arr, id, cb){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].id === id){
+      cb(arr[i]);
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
